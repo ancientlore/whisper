33d65d6db2205fd6627f9050756d739ad83efa19
Michael Lore <mike.lore@sap.com>
2020-01-07 22:58:08 -0500

    Template cache code, not used

--- cache.go
+++ cache.go
@@ -6,6 +6,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"html/template"
+	"io"
 	"net/url"
 	"strconv"
 	"time"
@@ -24,6 +25,8 @@ var (
 	dirListingCacheDuration time.Duration
 	markdownCache           *groupcache.Group
 	markdownCacheDuration   time.Duration
+	templateCache           *groupcache.Group
+	templateCacheDuration   time.Duration
 )
 
 func initGroupCache() {
@@ -135,4 +138,52 @@ func cachedRenderMarkdown(filename string) (*frontMatter, template.HTML, time.Ti
 		return nil, "", d.ModTime, fmt.Errorf("cachedRenderMarkdown: %w", err)
 	}
 	return d.FrontMatter, d.Content, d.ModTime, nil
+}
+
+type ctxKey string
+
+func initTemplateCache(cacheBytes int64, cacheDuration time.Duration) {
+	templateCacheDuration = cacheDuration
+	templateCache = groupcache.NewGroup("executeTemplate", cacheBytes, groupcache.GetterFunc(
+		func(ctx context.Context, key string, dest groupcache.Sink) error {
+			q, err := url.ParseQuery(key)
+			if err != nil {
+				return fmt.Errorf("executeTemplate group: %w", err)
+			}
+			// log.Printf("Calling executeTemplate %s", q.Encode())
+			var (
+				buf bytes.Buffer
+			)
+			data := ctx.Value(ctxKey("data")).(data)
+			// log.Print(data)
+			err = tpl.ExecuteTemplate(&buf, q.Get("templateName"), data)
+			if err != nil {
+				return fmt.Errorf("executeTemplate group: %w", err)
+			}
+
+			dest.SetBytes(buf.Bytes())
+			return nil
+		}))
+}
+
+func cachedExecuteTemplate(w io.Writer, name string, dat interface{}) error {
+	var (
+		buf groupcache.ByteView
+		q   = make(url.Values, 3)
+	)
+	q.Set("filename", dat.(data).Page.Filename)
+	t := quantize(time.Now(), templateCacheDuration, dat.(data).Page.Filename)
+	q.Set("t", strconv.FormatInt(t, 10))
+	q.Set("templateName", name)
+	ctx := context.WithValue(context.Background(), ctxKey("data"), dat)
+	// log.Printf("cachedExecuteTemplate %s", q.Encode())
+	err := templateCache.Get(ctx, q.Encode(), groupcache.ByteViewSink(&buf))
+	if err != nil {
+		return fmt.Errorf("cachedExecuteTemplate: %w", err)
+	}
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		return fmt.Errorf("cachedExecuteTemplate: %w", err)
+	}
+	return nil
 }
