dc8bf918ca13cc2b3c3a5749bb2e7c59369c3981
Michael Lore <mike.lore@sap.com>
2022-02-02 11:25:41 -0500

    Improve documentation

--- cachefs/fs.go
+++ cachefs/fs.go
@@ -1,3 +1,24 @@
+/*
+cachefs implements a read-only cache around a fs.FS, using groupcache.
+
+Using cachefs is straightforward:
+
+	// Setup groupcache (in this example with no peers)
+	groupcache.RegisterPeerPicker(func() groupcache.PeerPicker { return groupcache.NoPeers{} })
+
+	// Create the cached file system with group name "groupName", a 10MB cache, and a ten second expiration
+	cachedFileSystem := cachefs.New(os.DirFS("."), "groupName", 10*1024*1024, 10*time.Second)
+
+	// Use the file system as usual...
+
+cachefs "wraps" the underlying file system with caching. You can specify groupcache parameters - the group name
+and the cache size.
+
+groupcache does not support expiration, but cachefs supports quantizing values so that expiration happens
+around the expiration duration provided. Expiration can be disabled by specifying 0 for the duration.
+
+See https://pkg.go.dev/github.com/golang/groupcache for more information on groupcache.
+*/
 package cachefs
 
 import (
