cbc4f6cb1d5001c40285b7f64c13b6e090ad3f35
Michael Lore <michael.lore@gmail.com>
2023-02-04 15:00:55 -0500

    build script

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM golang:1.20 as builder
+ARG GO_VERSION=1.20
+ARG IMG_VERSION=1.20
+
+FROM golang:${GO_VERSION} as builder
 WORKDIR /go/src/github.com/ancientlore/whisper
 COPY . .
 RUN go version
 RUN CGO_ENABLED=0 GOOS=linux GO111MODULE=on go get .
 RUN CGO_ENABLED=0 GOOS=linux GO111MODULE=on go install
 
-FROM ancientlore/goimg:1.20
+FROM ancientlore/goimg:${IMG_VERSION}
 COPY --from=builder /go/bin/whisper /usr/local/bin/whisper
 COPY example/static/dog.png /home/www/whisper.png
 COPY example/.index-docker.md /home/www/index.md
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GO_VERSION=$(go env GOVERSION | cut -b "3-")
+GO_MAJOR_VERSION=$(cut -d '.' -f 1,2 <<< "$GO_VERSION")
+TAG=$(git tag | sort -V | tail -1)
+
+echo
+echo Go version is $GO_VERSION, major version is $GO_MAJOR_VERSION
+echo Tag is $TAG
+
+echo
+echo Building ancientlore/whisper:$TAG
+docker build --build-arg GO_VERSION=$GO_VERSION --build-arg IMG_VERSION=$GO_MAJOR_VERSION -t ancientlore/whisper:$TAG . || return 1
+
+echo
+echo Pushing ancientlore/whisper:$TAG
+docker push ancientlore/whisper:$TAG
+
+echo
+echo Tagging ancientlore.registry.cpln.io/whisper:$TAG
+docker tag ancientlore/whisper:$TAG ancientlore.registry.cpln.io/whisper:$TAG
+
+echo
+echo Pushing ancientlore.registry.cpln.io/webull:$TAG
+docker push ancientlore.registry.cpln.io/whisper:$TAG
