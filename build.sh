3a45bf26c4013d168a6c48d28b3cdfc453b42b4e
Michael Lore <michael.lore@gmail.com>
2023-06-16 15:27:32 -0400

    Confirm before push

--- build.sh
+++ build.sh
@@ -10,24 +10,26 @@ echo Tag is $TAG
 
 echo
 echo Building ancientlore/whisper:$TAG
-docker build --build-arg GO_VERSION=$GO_VERSION --build-arg IMG_VERSION=$GO_MAJOR_VERSION -t ancientlore/whisper:$TAG . || return 1
+docker build --build-arg GO_VERSION=$GO_VERSION --build-arg IMG_VERSION=$GO_MAJOR_VERSION -t ancientlore/whisper:$TAG . || exit 1
+
+gum confirm "Push?" || exit 1
 
 echo
 echo Pushing ancientlore/whisper:$TAG
-docker push ancientlore/whisper:$TAG
+docker push ancientlore/whisper:$TAG || exit 1
 
 echo
 echo Tagging ancientlore/whisper:latest
-docker tag ancientlore/whisper:$TAG ancientlore/whisper:latest
+docker tag ancientlore/whisper:$TAG ancientlore/whisper:latest || exit 1
 
 echo
 echo Pushing ancientlore/whisper:latest
-docker push ancientlore/whisper:latest
+docker push ancientlore/whisper:latest || exit 1
 
 echo
 echo Tagging ancientlore.registry.cpln.io/whisper:$TAG
-docker tag ancientlore/whisper:$TAG ancientlore.registry.cpln.io/whisper:$TAG
+docker tag ancientlore/whisper:$TAG ancientlore.registry.cpln.io/whisper:$TAG || exit 1
 
 echo
 echo Pushing ancientlore.registry.cpln.io/whisper:$TAG
-docker push ancientlore.registry.cpln.io/whisper:$TAG
+docker push ancientlore.registry.cpln.io/whisper:$TAG || exit 1
